a984055f347ad5f8b114f34cfb85c2b543333aff
Gideon <twumgideonasare@gmail.com>
2024-06-17 18:43:34 -0700

    update

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -6,10 +6,10 @@ const Services = () => {
         <div className="min-h-screen bg-black mt-24  flex items-center justify-center">
         <div className="flex gap-2 ">
            <img className="" src="bag.png" alt=""/>
-           <p className="text-[#FFC700] text-[4rem]  font-bold top-[86rem] left-[25rem] absolute">Our <br/> Services</p>
+           <p className="text-[#FFC700] text-[4rem]  font-bold top-[90rem] left-[25rem] absolute">Our <br/> Services</p>
         </div> 
 
-        <div className="absolute top-[75rem] left-[6rem]">
+        <div className="absolute top-[80rem] left-[6rem]">
         <p className="text-white text-[15px] mb-[4px]">Transparency and Decentralisation</p>
           <ul className="text-white text-[15px]">
               <p>- Unbreakable Trust: Leverage the power of blockchain <br/> for an immutable and transparent claims process. </p>
@@ -32,7 +32,7 @@ const Services = () => {
           </ul>
       </div>
 
-      <div className="absolute top-[78rem] right-[9rem]">
+      <div className="absolute top-[82rem] right-[9rem]">
         <p className="text-white text-[15px] mb-[6px]">Security and Efficiency:</p> 
           <ul className="text-white text-[13px]">
               <p>- Zero-Knowledge Proof: zkpass technology ensures your   <br/>personal data remains private while verifying your <br/>eligibility for coverage. </p>
